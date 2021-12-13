a0b4ab48deb9c2017df2b4bed0d2cf326cd1a3ea
daniildn <dosindaniilm@gmail.com>
2021-12-14 01:55:51 +0300

    learned loops

---
+++ loops.py
@@ -0,0 +1,12 @@
+while (True):
+    name = input("who invented python? ")
+    if name == "guido":
+        print("winner")
+        break
+    break
+
+num = 0
+while num <= 10:
+    if num % 2 == 0:
+        print(num)
+    num += 1
