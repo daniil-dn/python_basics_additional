79417b3568ceede6c995ea9a2f152c20c72baa34
daniildn <dosindaniilm@gmail.com>
2021-12-14 02:28:34 +0300

    fixed the compare error with 40age

--- hw1.py
+++ hw1.py
@@ -3,7 +3,7 @@ while True:
     if users_num >= 0 and users_num <= 10:
         print(users_num ** 2)
     else:
-        continue
+        break
 name = input("enter your name")
 surname = input("enter your surname")
 age = int(input("enter your age"))
@@ -11,7 +11,7 @@ weight = int(input("enter your weight"))
 
 if age <= 30 and weight <= 120 and weight >= 50:
     print("you are in good fit")
-elif age > 30 and weight > 120 and weight < 50:
+elif age > 30 and age <= 40 and weight > 120 and weight < 50:
     print("you should work on yourself")
 elif age > 40 and weight > 120 and weight < 50:
     print("You have to visit a doctor")
