679367bd7b037e933c824e0a182ef66ab321bda9
daniildn <dosindaniilm@gmail.com>
2021-12-15 15:10:14 +0300

    completed hw2

--- hw3.py
+++ hw3.py
@@ -9,3 +9,31 @@ def name_age_town(name, age, town):
 def biggest_among_nums(*args):
     print(sorted(args, reverse=True)[0])
 
+
+def damage_armor_calculation(damage, armor_ratio):
+    return damage / armor_ratio
+
+
+def attack(attacker, survivor, armor=1.0):
+    damage = damage_armor_calculation(attacker["damage"], armor)
+
+    survivor["health"] -= damage
+
+
+player = {
+    "name": "Max",
+    "health": 100,
+    "damage": 50,
+    'armor': 1.2,
+}
+enemy = {
+    "name": "the enemy - your terrible dreammmm))) ",
+    "health": 100,
+    "damage": 50
+}
+print(player)
+print(enemy)
+print("before an attack")
+attack(enemy, player, player["armor"])
+print(player)
+print(enemy)
