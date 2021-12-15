93d22226f7777e62844cb77ca05067e190e269ab
daniildn <dosindaniilm@gmail.com>
2021-12-15 07:55:02 +0300

    add a map func and a lymbda for it)

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-def get_sep(sep_sign, sep_len):
+def get_sep(sep_sign, *sep_len):
     # print(sep_sign * sep_len)
     return sep_sign * sep_len
 
@@ -10,8 +10,22 @@ def hello(who):
 hello('max')
 
 
-def greeting(who="Nobody", say= "hello"):
+def greeting(who="Nobody", say="hello"):
     print(say, who)
 
 
 greeting(say='who are you?', who="leo")
+
+
+def filter(nums, func):
+    result = []
+    for num in nums:
+        if func(num):
+            result.append(num)
+    return result
+
+
+numbers = [1, 2, 34, 5, 3, 2, 6]
+
+print(filter(numbers, lambda x: x % 2 == 0))
+list(map(lambda x: x**2, numbers))
