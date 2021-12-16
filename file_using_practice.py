7461a769fdff0a3050ce899d6f6a1ece6569a480
daniildn <dosindaniilm@gmail.com>
2021-12-16 15:34:22 +0300

    learning file using

---
+++ file_using_practice.py
@@ -0,0 +1,23 @@
+f_1 = open('first.txt', 'w')
+
+# f_1.write("Write hello world \n")
+# f_1.write("Write hello world")
+
+f_1.writelines(['hello\n', 'Python'])
+
+f_1 = open('first.txt', 'r')
+# print(type(f_1))
+#
+print(f_1.read())
+print(f_1.readlines())
+for line in f_1:
+    # pass
+    print(line.replace('\n', ' '))
+    # line = 'adf'
+#
+
+with open('first.txt', 'r') as file:
+    for line in file:
+        # pass
+        print(line.replace('\n', ' '))
+f_1.close()
--- hw4/main.py
+++ hw4/main.py
@@ -4,3 +4,4 @@ from pack1_test.modl import rm_several_dirs, mk_several_dirs, rand_list_item
 
 rm_several_dirs(5)
 print(rand_list_item([1,2,3,4,5,6,7]))
+
