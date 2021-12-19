f143a7bd11f4c8089f51eaef0a997a33ec2defb0
daniildn <dosindaniilm@gmail.com>
2021-12-19 17:34:41 +0300

    added the fourth task to hw 6

--- hw6/main.py
+++ hw6/main.py
@@ -24,11 +24,28 @@ filtered_nums_list = [n for n in nums if n > 0 and n % 3 == 0 and n % 4 != 0]
 
 # print(filtered_nums_list)
 
+# 3. Напишите функцию которая принимает на вход список.
+# Функция создает из этого списка новый список из квадратных корней чисел (если число положительное) и самих чисел (если число отрицательное)
+# и возвращает результат (желательно применить генератор и тернарный оператор при необходимости).
+# В результате работы функции исходный список не должен измениться
+
 def sqrt_lists_nums(ls=None):
     if ls is None:
         pass
-    result = [n**2 if n>0 else n for n in ls]
+    result = [n ** 2 if n > 0 else n for n in ls]
     return result
 
 
-print(sqrt_lists_nums(nums))
+# print(sqrt_lists_nums(nums))
+# print(nums)
+
+def sqrt_but_not_for_13(num):
+    if num == 13:
+        raise Exception("num is 13 - i can't  square this number")
+    else:
+        return num ** 2
+
+try:
+    print(sqrt_but_not_for_13(13))
+except Exception:
+    pass
