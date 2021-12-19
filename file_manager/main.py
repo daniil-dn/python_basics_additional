b4b58820f40365192ab2f11c84f85b1545be2f65
daniildn <dosindaniilm@gmail.com>
2021-12-19 20:13:31 +0300

    added func for creating folder

--- file_manager/main.py
+++ file_manager/main.py
@@ -1,4 +1,19 @@
+import os
+
+
 def create_file(name, text=None):
     with open(name, 'w', encoding='utf-8') as f:
         if text:
             f.write(text)
+
+
+def create_folder(name):
+    try:
+        os.mkdir(name)
+    except FileExistsError:
+        print('The folder with such name already exists.')
+
+
+if __name__ == '__main__':
+    create_file('text.dat')
+    create_folder('new_f')
